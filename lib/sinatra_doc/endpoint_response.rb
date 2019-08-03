f267c965ab9d214606c79ddf5bd900da517971b4
Joe Grigg <joe@Joes-MacBook-Pro.local>
2019-08-03 12:32:38 +0100

    Added support for nested response properties

--- lib/sinatra_doc/endpoint_response.rb
+++ lib/sinatra_doc/endpoint_response.rb
@@ -1,16 +1,13 @@
 module SinatraDoc
   class Endpoint
     class Response
+      include PropMethods
+
       attr_reader :code
 
       def initialize(code)
         @code = code
         @props = []
-      end
-
-      def prop(name, type, description)
-        prop = Prop.new(name, type, description)
-        @props << prop
       end
     end
   end
--- lib/sinatra_doc/endpoint_response_prop.rb
+++ lib/sinatra_doc/endpoint_response_prop.rb
@@ -1,13 +1,24 @@
 module SinatraDoc
   class Endpoint
     class Response
+      module PropMethods
+        def prop(name, type, description, &block)
+          prop = Prop.new(name, type, description)
+          prop.instance_eval(&block) if block_given? && type == :object
+          @props << prop
+        end
+      end
+
       class Prop
+        include PropMethods
+
         attr_reader :name, :type, :description
 
         def initialize(name, type, description)
           @name = name
           @type = type
           @description = description
+          @props = []
         end
       end
     end
