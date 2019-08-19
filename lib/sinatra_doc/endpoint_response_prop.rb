54ed7231d517a1e8d18ff656d508257839d7daa0
Joe Grigg <account@joegrigg.com>
2019-08-19 10:39:16 +0100

    Added array support to response props

--- lib/sinatra_doc/endpoint_response_prop.rb
+++ lib/sinatra_doc/endpoint_response_prop.rb
@@ -2,9 +2,14 @@ module SinatraDoc
   class Endpoint
     class Response
       module PropMethods
-        def prop(name, type, description, &block)
-          prop = Prop.new(name, type, description)
-          prop.instance_eval(&block) if block_given? && type == :object
+        def prop(name, type, description = nil, of: nil, &block)
+          prop = Prop.new(name, type, description, of: of)
+          raise ArgumentError, "Param `of` must be defined when `type` is array" if type == :array && of.nil?
+          if block_given?
+            raise ArgumentError, "Block given but not being used" unless type == :object ||
+                                                                         (type == :array && of == :object)
+            prop.instance_eval(&block)
+          end
           @props << prop
         end
       end
@@ -14,10 +19,11 @@ module SinatraDoc
 
         attr_reader :name, :type, :description
 
-        def initialize(name, type, description)
+        def initialize(name, type, description, of: nil)
           @name = name
           @type = type
           @description = description
+          @of = of
           @props = []
         end
       end
