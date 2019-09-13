1cedf82ded0138d9f472cb2e8b62d4b379a174b9
Joe Grigg <account@joegrigg.com>
2019-09-13 15:34:23 +0100

    Added default swagger security definition

--- lib/sinatra_doc/adapters/swagger.rb
+++ lib/sinatra_doc/adapters/swagger.rb
@@ -13,7 +13,9 @@ module SinatraDoc
             host: SinatraDoc.host,
             schemes: [ :http ],
             tags: SinatraDoc.tags.map(&:compact),
-            paths: adapt_array(SinatraDoc.endpoints, deep: true)
+            paths: adapt_array(SinatraDoc.endpoints, deep: true),
+            securityDefinitions: { JWT: { description: "", type: "apiKey", name: "Authorization", in: "header" } },
+            security: [ { "JWT": [] } ]
           }
         end
 
